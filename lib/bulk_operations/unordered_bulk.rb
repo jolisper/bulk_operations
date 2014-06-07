3b9dc50913240de441c41be164d9f62a151fd821
Jorge L Pérez <jolisper@gmail.com>
2014-06-07 15:37:07 -0300

    Refactor, more files with less code

---
+++ lib/bulk_operations/unordered_bulk.rb
@@ -0,0 +1,33 @@
+module BulkOperations
+  class UnorderedBulkProxy < BulkProxy
+    def do_execute
+      result = Hash.new
+      threads = []
+
+      @operation_stack.size
+
+      @operation_stack.each do |operation|
+        name, *args, block = *operation
+        threads << spawn_thread(name, args, block, result)
+      end
+
+      threads.each { |thread| thread.join }
+
+      StructResult.new result
+    end
+
+    def spawn_thread(name, args, block, result)
+      thread = Thread.new do
+        begin
+          call_result = @proxied_object.send(name, *args)
+          result[name] = { ok: true, result: call_result }
+        rescue => exception
+          result[name] = { ok: false, result: exception }
+        end
+      end
+
+      thread
+    end
+
+  end
+end
